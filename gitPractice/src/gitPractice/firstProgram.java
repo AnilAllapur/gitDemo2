599bfeb893b7e8ede05793ca814afd6e360ddb1c
B Tester <btester@gmail.com>
2025-07-12 16:08:59 +0530

    second commit By Tester B

--- gitPractice/src/gitPractice/firstProgram.java
+++ gitPractice/src/gitPractice/firstProgram.java
@@ -5,6 +5,7 @@ public class firstProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Day1 Testing is done by A person");
+System.out.println("Day2 Testing is done by B person");
 	}
 
 }
--- gitPractice/src/gitPractice/secondProgram.java
+++ gitPractice/src/gitPractice/secondProgram.java
@@ -5,6 +5,8 @@ public class secondProgram {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Day1 Reporting is done by A person");
+		System.out.println("Day2 Reporting is done by B person");
+
 	}
 
 }
